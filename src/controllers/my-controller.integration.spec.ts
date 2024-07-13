fca47f7dabdd811b54b93a619ec083a365d9c6ce
Birame S <birame.sene@zenika.com>
2024-07-13 17:10:59 +0200

    test: adding use cases

--- src/controllers/my-controller.integration.spec.ts
+++ src/controllers/my-controller.integration.spec.ts
@@ -36,6 +36,88 @@ describe('MyController Integration Tests', () => {
 		await fastify.close();
 	});
 
+	it('should notify lead time when normal product is NOT available', async () => {
+		const client = supertest(fastify.server);
+		const allProducts = [
+			{
+				leadTime: 10, available: 0, type: 'NORMAL', name: 'USB Dongle',
+			},
+		];
+		const orderId = await database.transaction(async tx => {
+			const productList = await tx.insert(products).values(allProducts).returning({productId: products.id});
+			const [order] = await tx.insert(orders).values([{}]).returning({orderId: orders.id});
+			await tx.insert(ordersToProducts).values(productList.map(p => ({orderId: order!.orderId, productId: p.productId})));
+			return order!.orderId;
+		});
+
+		await client.post(`/orders/${orderId}/processOrder`).expect(200).expect('Content-Type', /application\/json/);
+
+		expect(notificationServiceMock.sendDelayNotification).toHaveBeenCalledWith(10, 'USB Dongle');
+	});
+
+	it('should notify lead time when seasonal product (in season) is NOT available', async () => {
+		const client = supertest(fastify.server);
+		const d = 24 * 60 * 60 * 1000;
+
+		const allProducts = [
+			{
+				leadTime: 15, available: 0, type: 'SEASONAL', name: 'Watermelon', seasonStartDate: new Date(Date.now() - (2 * d)), seasonEndDate: new Date(Date.now() + (58 * d)),
+			}
+		];
+		const orderId = await database.transaction(async tx => {
+			const productList = await tx.insert(products).values(allProducts).returning({productId: products.id});
+			const [order] = await tx.insert(orders).values([{}]).returning({orderId: orders.id});
+			await tx.insert(ordersToProducts).values(productList.map(p => ({orderId: order!.orderId, productId: p.productId})));
+			return order!.orderId;
+		});
+
+		await client.post(`/orders/${orderId}/processOrder`).expect(200).expect('Content-Type', /application\/json/);
+
+		expect(notificationServiceMock.sendDelayNotification).toHaveBeenCalledWith(15, 'Watermelon');
+	});
+
+	it('should notify out of stock when seasonal product (out of season) is NOT available', async () => {
+		const client = supertest(fastify.server);
+		const d = 24 * 60 * 60 * 1000;
+
+		const allProducts = [
+			{
+				leadTime: 13, available: 0, type: 'SEASONAL', name: 'Grapes', seasonStartDate: new Date(Date.now() + (180 * d)), seasonEndDate: new Date(Date.now() + (240 * d)),
+			}
+		];
+		const orderId = await database.transaction(async tx => {
+			const productList = await tx.insert(products).values(allProducts).returning({productId: products.id});
+			const [order] = await tx.insert(orders).values([{}]).returning({orderId: orders.id});
+			await tx.insert(ordersToProducts).values(productList.map(p => ({orderId: order!.orderId, productId: p.productId})));
+			return order!.orderId;
+		});
+
+		await client.post(`/orders/${orderId}/processOrder`).expect(200).expect('Content-Type', /application\/json/);
+
+		expect(notificationServiceMock.sendOutOfStockNotification).toHaveBeenCalledWith('Grapes');
+	});
+
+	it('should notify expiration when expirable product has passed', async () => {
+		const client = supertest(fastify.server);
+		const d = 24 * 60 * 60 * 1000;
+
+		const allProducts = [
+			{
+				leadTime: 90, available: 9, type: 'EXPIRABLE', name: 'Milk', expiryDate: new Date(Date.now() - (2 * d)),
+			}
+		];
+		const orderId = await database.transaction(async tx => {
+			const productList = await tx.insert(products).values(allProducts).returning({productId: products.id});
+			const [order] = await tx.insert(orders).values([{}]).returning({orderId: orders.id});
+			await tx.insert(ordersToProducts).values(productList.map(p => ({orderId: order!.orderId, productId: p.productId})));
+			return order!.orderId;
+		});
+
+		await client.post(`/orders/${orderId}/processOrder`).expect(200).expect('Content-Type', /application\/json/);
+
+		expect(notificationServiceMock.sendExpirationNotification).toHaveBeenCalled();
+	});
+
 	it('ProcessOrderShouldReturn', async () => {
 		const client = supertest(fastify.server);
 		const allProducts = createProducts();
